7fe4657562f6c5a0c7e0aee90b90cb1122d69c4f
Rakesh Kumar <44721620+imraklr@users.noreply.github.com>
2024-09-12 08:56:57 +0530

    fix: adds a snippet in ps1 file to create output
    description: The command to create output folder to write the output was
    added.

--- compile.ps1
+++ compile.ps1
@@ -1,1 +1,7 @@
-g++ -std=c++23 src/source/*.cpp -o src/target/output/main
+# Create the target/output directory if it doesn't exist
+$targetDir = "src/target/output"
+if (-not (Test-Path -Path $targetDir)) {
+    New-Item -ItemType Directory -Path $targetDir
+}
+
+g++ -std=c++23 src/source/*.cpp -o $targetDir/main
